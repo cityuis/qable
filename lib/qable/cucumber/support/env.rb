c3ce78f29f2e3fe5b99703e0a19013e3c6941629
Guillermo Iguaran <guilleiguaran@gmail.com>
2017-09-09 09:45:19 -0500

    Use driver specified in config file

--- lib/qable/cucumber/support/env.rb
+++ lib/qable/cucumber/support/env.rb
@@ -1,11 +1,12 @@
 require "minitest"
 require "cucumber"
 require "capybara/cucumber"
+require "selenium/webdriver"
 
 config = YAML.load_file(File.join(Dir.pwd, "config.yml"))
 
-Capybara.default_driver = :selenium_chrome_headless
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.default_driver = config["driver"].to_sym
+Capybara.javascript_driver = config["driver"].to_sym
 Capybara.run_server = false
 Capybara.app_host = config["site"] || "http://localhost:3000"
 
